277220f0f165884ebc8db0fbd556c4085c84b8ea
DM <cryptofanonawindyday@gmail.com>
2018-03-29 22:44:45 +0100

    code cleanup

--- src/app/components/backup/backup.component.ts
+++ src/app/components/backup/backup.component.ts
@@ -1,18 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-backup',
-  templateUrl: './backup.component.html',
-  styleUrls: ['./backup.component.scss']
+    selector: 'app-backup',
+    templateUrl: './backup.component.html',
+    styleUrls: ['./backup.component.scss']
 })
+
 export class BackupComponent implements OnInit {
 
-  items: any[] = [{id: 1, value: 'item1'}, {id: 2, value: 'item2tz1KjhfuiS9gjrQ6DodL3cfLDWkGzGvUoQux'}, {id: 3, value: 'item3'}];
-  myModel: any = this.items[1];
+    constructor() { }
 
-  constructor() { }
+    ngOnInit() {
 
-  ngOnInit() {
-  }
+    }
 
 }
--- src/app/components/send/send.component.ts
+++ src/app/components/send/send.component.ts
@@ -8,169 +8,181 @@ import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 import { KeyPair } from '../../interfaces';
 
 @Component({
-  selector: 'app-send',
-  templateUrl: './send.component.html',
-  encapsulation: ViewEncapsulation.None,
-  styleUrls: ['./send.component.scss']
+    selector: 'app-send',
+    templateUrl: './send.component.html',
+    encapsulation: ViewEncapsulation.None,
+    styleUrls: ['./send.component.scss']
 })
 export class SendComponent implements OnInit {
-  @ViewChild('modal1') modal1: TemplateRef<any>;
-  accounts = null;
-  @Input() activePkh: string;
-  @Input() accounts2: any;
-  activeAccount = null;
-  toPkh: string;
-  amount: string;
-  fee: string;
-  password: string;
-  pwdValid: string;
-  formInvalid = '';
-  sendResponse: string;
-  // closeResult: string;
-  modalRef1: BsModalRef;
-  modalRef2: BsModalRef;
-  modalRef3: BsModalRef;
+    @ViewChild('modal1') modal1: TemplateRef<any>;
+    accounts = null;
+    @Input() activePkh: string;
+    @Input() accounts2: any;
+    activeAccount = null;
+    toPkh: string;
+    amount: string;
+    fee: string;
+    password: string;
+    pwdValid: string;
+    formInvalid = '';
+    sendResponse: string;
 
-  constructor(
-    private modalService: BsModalService,
-    private walletService: WalletService,
-    private messageService: MessageService,
-    private transactionService: TransactionService
-  ) { }
+    modalRef1: BsModalRef;
+    modalRef2: BsModalRef;
+    modalRef3: BsModalRef;
 
-  ngOnInit() {
-    if (this.accounts2) {
-      this.activeAccount = this.accounts2;
-      console.log('activeAccount', this.activeAccount);
-      console.log('activeAccount.pkh', this.activeAccount.pkh);
-      console.log('activeAccount.balance.balanceXTZ', this.activeAccount.balance.balanceXTZ / 1000000);
+    constructor(
+        private modalService: BsModalService,
+        private walletService: WalletService,
+        private messageService: MessageService,
+        private transactionService: TransactionService
+    ) { }
 
-      console.log('accounts2', this.accounts2);
-    }
+    ngOnInit() {
+        if (this.accounts2) {
+            this.activeAccount = this.accounts2;
+            console.log('activeAccount', this.activeAccount);
+            console.log('activeAccount.pkh', this.activeAccount.pkh);
+            console.log('activeAccount.balance.balanceXTZ', this.activeAccount.balance.balanceXTZ / 1000000);
 
-    if (this.walletService.wallet) {
-      this.init();
-    }
-  }
-
-  init() {
-    this.accounts = this.walletService.wallet.accounts;
-  }
-
-  showAccountBalance(accountPkh: string) {
-    let accountBalance: number;
-    let accountBalanceString: string;
-    let index;
-    // finding the index
-    index = this.accounts.findIndex(account => account.pkh === accountPkh);
-
-    accountBalance = this.accounts[index].balance.balanceXTZ / 1000000;
-    accountBalanceString = this.numberWithCommas(accountBalance) + ' ꜩ';
-
-    return accountBalanceString;
-  }
-
-  numberWithCommas(x: number) {
-    let parts: Array<string> = [];
-    parts = x.toString().split('.');
-    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-    return parts.join('.');
-}
-
-sendEntireBalance(accountPkh: string, event: Event) {
-
-  event.stopPropagation();
-
-  let accountBalance: number;
-  let index;
-  // finding the index
-  index = this.accounts.findIndex(account => account.pkh === accountPkh);
-
-  accountBalance = this.accounts[index].balance.balanceXTZ / 1000000;
-
-  this.amount = accountBalance.toString();
-}
-
-  open1(template1: TemplateRef<any>) {
-    this.clearForm();
-    this.modalRef1 = this.modalService.show(template1, { class: 'first' });
-/*
-    this.modalRef1 = this.modalService.show(
-      template1,
-      Object.assign({}, { class: 'gray modal-lg' })
-    );
-*/
-  }
-  open2(template: TemplateRef<any>) {
-    this.formInvalid = this.invalidInput();
-    if (!this.formInvalid) {
-      if (!this.amount) { this.amount = '0'; }
-      if (!this.fee) { this.fee = '0'; }
-      this.close1();
-      this.modalRef2 = this.modalService.show(template, { class: 'second' });
-    }
-  }
-  async open3(template: TemplateRef<any>) {
-    const pwd = this.password;
-    this.password = '';
-    let keys;
-    if (keys = this.walletService.getKeys(pwd)) {
-      this.pwdValid = '';
-      this.close2();
-      this.modalRef3 = this.modalService.show(template, { class: 'third' });
-      this.sendTransaction(keys);
-    } else {
-      this.pwdValid = 'Wrong password!';
-    }
-  }
-  close1() {
-    this.modalRef1.hide();
-    this.modalRef1 = null;
-  }
-  close2() {
-    this.modalRef2.hide();
-    this.modalRef2 = null;
-  }
-  close3() {
-    this.modalRef3.hide();
-    this.modalRef3 = null;
-  }
-
-async sendTransaction(keys: KeyPair) {
-      const toPkh = this.toPkh;
-      let amount = this.amount;
-      let fee = this.fee;
-      this.toPkh = '';
-      this.amount = '';
-      this.fee = '';
-      if (!amount) { amount = '0'; }
-      if (!fee) { fee = '0'; }
-      setTimeout(async () => {
-        if (await this.transactionService.sendTransaction(keys, this.activePkh, toPkh, Number(amount), Number(fee) * 100)) {
-          this.sendResponse = 'success';
-        } else {
-          this.sendResponse = 'failure';
+            console.log('accounts2', this.accounts2);
         }
-      }, 100);
-  }
-  clearForm() {
-    this.toPkh = '';
-    this.amount = '';
-    this.fee = '';
-    this.password = '';
-    this.pwdValid = '';
-    this.formInvalid = '';
-    this.sendResponse = '';
-  }
-  invalidInput(): string {
-    if (!this.toPkh || this.toPkh.length !== 36) {
-      return 'invalid receiver address';
-    } else if (!Number(this.amount) && this.amount && this.amount !== '0') {
-      return 'invalid amount';
-    } else if (!Number(this.fee) && this.fee && this.fee !== '0') {
-      return 'invalid fee';
-    } else {
-      return '';
+
+        if (this.walletService.wallet) {
+            this.init();
+        }
     }
-  }
+
+    init() {
+        this.accounts = this.walletService.wallet.accounts;
+    }
+
+    showAccountBalance(accountPkh: string) {
+        let accountBalance: number;
+        let accountBalanceString: string;
+        let index;
+        // finding the index
+        index = this.accounts.findIndex(account => account.pkh === accountPkh);
+
+        accountBalance = this.accounts[index].balance.balanceXTZ / 1000000;
+        accountBalanceString = this.numberWithCommas(accountBalance) + ' ꜩ';
+
+        return accountBalanceString;
+    }
+
+    numberWithCommas(x: number) {
+        let parts: Array<string> = [];
+        parts = x.toString().split('.');
+        parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        return parts.join('.');
+    }
+
+    sendEntireBalance(accountPkh: string, event: Event) {
+        event.stopPropagation();
+
+        let accountBalance: number;
+        let index;
+
+        // finding the index
+        index = this.accounts.findIndex(account => account.pkh === accountPkh);
+
+        accountBalance = this.accounts[index].balance.balanceXTZ / 1000000;
+
+        this.amount = accountBalance.toString();
+    }
+
+    open1(template1: TemplateRef<any>) {
+        this.clearForm();
+        this.modalRef1 = this.modalService.show(template1, { class: 'first' });
+        /*
+        this.modalRef1 = this.modalService.show(
+        template1,
+        Object.assign({}, { class: 'gray modal-lg' })
+        );
+        */
+    }
+    open2(template: TemplateRef<any>) {
+        this.formInvalid = this.invalidInput();
+        if (!this.formInvalid) {
+            if (!this.amount) { this.amount = '0'; }
+            if (!this.fee) { this.fee = '0'; }
+            this.close1();
+            this.modalRef2 = this.modalService.show(template, { class: 'second' });
+        }
+    }
+    async open3(template: TemplateRef<any>) {
+        const pwd = this.password;
+        this.password = '';
+        let keys;
+        if (keys = this.walletService.getKeys(pwd)) {
+            this.pwdValid = '';
+            this.close2();
+            this.modalRef3 = this.modalService.show(template, { class: 'third' });
+            this.sendTransaction(keys);
+        } else {
+            this.pwdValid = 'Wrong password!';
+        }
+    }
+
+    close1() {
+        this.modalRef1.hide();
+        this.modalRef1 = null;
+    }
+
+    close2() {
+        this.modalRef2.hide();
+        this.modalRef2 = null;
+    }
+
+    close3() {
+        this.modalRef3.hide();
+        this.modalRef3 = null;
+    }
+
+    async sendTransaction(keys: KeyPair) {
+
+        const toPkh = this.toPkh;
+        let amount = this.amount;
+        let fee = this.fee;
+        this.toPkh = '';
+        this.amount = '';
+        this.fee = '';
+
+        if (!amount) {
+            amount = '0';
+        }
+
+        if (!fee) {
+            fee = '0';
+        }
+
+        setTimeout(async () => {
+            if (await this.transactionService.sendTransaction(keys, this.activePkh, toPkh, Number(amount), Number(fee) * 100)) {
+                this.sendResponse = 'success';
+            } else {
+                this.sendResponse = 'failure';
+            }
+        }, 100);
+    }
+    clearForm() {
+        this.toPkh = '';
+        this.amount = '';
+        this.fee = '';
+        this.password = '';
+        this.pwdValid = '';
+        this.formInvalid = '';
+        this.sendResponse = '';
+    }
+    invalidInput(): string {
+
+        if (!this.toPkh || this.toPkh.length !== 36) {
+            return 'invalid receiver address';
+        } else if (!Number(this.amount) && this.amount && this.amount !== '0') {
+            return 'invalid amount';
+        } else if (!Number(this.fee) && this.fee && this.fee !== '0') {
+            return 'invalid fee';
+        } else {
+            return '';
+        }
+    }
 }
