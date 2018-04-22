c1b58244f64a62c6d6d95e50cec62476544881f2
DM <cryptofanonawindyday@gmail.com>
2018-04-22 23:52:49 +0100

    Backup UI

--- src/app/components/backup/backup.component.ts
+++ src/app/components/backup/backup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { WalletService } from '../../services/wallet.service';
 
 @Component({
@@ -7,12 +7,33 @@ import { WalletService } from '../../services/wallet.service';
     styleUrls: ['./backup.component.scss']
 })
 
-export class BackupComponent implements OnInit {
+export class BackupComponent implements OnInit, OnDestroy {
+    mySeed = '';
+    showFileButton = false;
+    saveSuccessfully = false;
 
     constructor(private walletService: WalletService) { }
 
     ngOnInit() {
 
+    }
+
+    decryptSeed() {
+        this.mySeed = 'item similar grow aim monster kick debris empty early pelican senior history creek impulse slim';
+    }
+
+    revealFileButton() {
+        this.showFileButton = true;
+    }
+
+    saveFile() {
+        this.saveSuccessfully = true;
+    }
+
+    ngOnDestroy() {
+        this.mySeed = '';
+        this.showFileButton = false;
+        this.saveSuccessfully = false;
     }
 
 }
