52c76fca09254af34b824281723b9075baa7ded4
DM <cryptofanonawindyday@gmail.com>
2018-05-13 19:25:30 +0100

    3-step broadcast

--- src/app/components/broadcast/broadcast.component.ts
+++ src/app/components/broadcast/broadcast.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-broadcast',
-  templateUrl: './broadcast.component.html',
-  styleUrls: ['./broadcast.component.scss']
+    selector: 'app-broadcast',
+    templateUrl: './broadcast.component.html',
+    styleUrls: ['./broadcast.component.scss']
 })
 export class BroadcastComponent implements OnInit {
 
-  constructor() { }
+    InputImportWalletFileStep1 = 'Choose file';
+    InputImportWalletFileStep2 = 'Choose file';
+    InputImportOperationFileStep2 = 'Choose file';
+    InputImportOperationFileStep3 = 'Choose file';
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    handleViewOnlyWalletFileInput(files: FileList) {
+        let walletFileInput = files.item(0);
+        this.InputImportWalletFileStep1 = walletFileInput.name;
+    }
+
+    handleWalletFileInput(files: FileList) {
+        let walletFileInput = files.item(0);
+        this.InputImportWalletFileStep2 = walletFileInput.name;
+    }
+
+    handleUnsignedOperationFileInput(files: FileList) {
+        let operationFileInput = files.item(0);
+        this.InputImportOperationFileStep2 = operationFileInput.name;
+    }
+
+    handleSignedOperationFileInput(files: FileList) {
+        let signedOperationFileInput = files.item(0);
+        this.InputImportOperationFileStep3 = signedOperationFileInput.name;
+    }
 
 }
