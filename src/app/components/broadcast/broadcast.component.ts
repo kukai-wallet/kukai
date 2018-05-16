8e966e295f6a01b0aa122046f44f363d7758004f
DM <cryptofanonawindyday@gmail.com>
2018-05-16 23:48:51 +0100

    broadcast completed

--- src/app/components/broadcast/broadcast.component.ts
+++ src/app/components/broadcast/broadcast.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+
+import { WalletService } from '../../services/wallet.service';
 import { OperationService } from '../../services/operation.service';
 import { MessageService } from '../../services/message.service';
-import { WalletService } from '../../services/wallet.service';
+import { ExportService } from '../../services/export.service';
 import { CoordinatorService } from '../../services/coordinator.service';
 
 @Component({
@@ -16,38 +18,50 @@ export class BroadcastComponent implements OnInit {
     InputImportOperationFileStep2 = 'Choose file';
     InputImportOperationFileStep3 = 'Choose file';
 
+    @Input() unsigned = '';
     @Input() signed = '';
+    @Input() pwd = '';
+    pwdPlaceholder = '';
 
     constructor(
+        private walletService: WalletService,
         private operationService: OperationService,
         private messageService: MessageService,
-        private walletService: WalletService,
+        private exportService: ExportService,
         private coordinatorService: CoordinatorService
     ) { }
 
     ngOnInit() {
+        if (this.walletService.wallet && this.walletService.isFullWallet()) {
+            this.init();
+        }
+    }
+
+    init() {
+        this.pwdPlaceholder = 'Password';
     }
 
     handleViewOnlyWalletFileInput(files: FileList) {
-        let walletFileInput = files.item(0);
+        const walletFileInput = files.item(0);
         this.InputImportWalletFileStep1 = walletFileInput.name;
     }
 
     handleWalletFileInput(files: FileList) {
-        let walletFileInput = files.item(0);
+        const walletFileInput = files.item(0);
         this.InputImportWalletFileStep2 = walletFileInput.name;
     }
 
+    /*
     handleUnsignedOperationFileInput(files: FileList) {
-        let operationFileInput = files.item(0);
+        const operationFileInput = files.item(0);
         this.InputImportOperationFileStep2 = operationFileInput.name;
     }
 
     handleSignedOperationFileInput(files: FileList) {
-        let signedOperationFileInput = files.item(0);
+        const signedOperationFileInput = files.item(0);
         this.InputImportOperationFileStep3 = signedOperationFileInput.name;
     }
-
+    */
     broadcast() {
         if (this.signed) {
             const signed = this.signed;
@@ -71,9 +85,11 @@ export class BroadcastComponent implements OnInit {
         }
     }
 
-    import(files: FileList) {
+    handleSignedOperationFileInput(files: FileList) {
         console.log('Files length: ' + files.length);
         const fileToUpload = files.item(0);
+        this.InputImportOperationFileStep3 = fileToUpload.name;
+
         const reader = new FileReader();
         reader.readAsText(fileToUpload);
 
@@ -85,6 +101,45 @@ export class BroadcastComponent implements OnInit {
                     this.signed = data.hex;
                 } else {
                     this.messageService.addWarning('Not an unsigned operation!');
+                }
+            } else {
+                this.messageService.addError('Failed to read file!');
+            }
+        };
+    }
+
+    sign() {
+        if (this.pwd) {
+          console.log('sign');
+          const pwd = this.pwd;
+          this.pwd = '';
+          const keys = this.walletService.getKeys(pwd);
+          if (keys) {
+            const signed = this.operationService.sign(this.unsigned, keys.sk);
+            this.signed = signed.sbytes;
+          }
+        }
+      }
+
+    download() {
+        this.exportService.downloadOperationData(this.signed, true);
+    }
+
+    handleUnsignedOperationFileInput(files: FileList) {
+        const fileToUpload = files.item(0);
+        this.InputImportOperationFileStep2 = fileToUpload.name;
+
+        const reader = new FileReader();
+        reader.readAsText(fileToUpload);
+
+        reader.onload = () => {
+            if (reader.result) {
+                const data = JSON.parse(reader.result);
+
+                if (data.signed === false && data.hex) {
+                    this.unsigned = data.hex;
+                } else {
+                this.messageService.addWarning('Not an unsigned operation!');
                 }
             } else {
                 this.messageService.addError('Failed to read file!');
