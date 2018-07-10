b979ed8b283db2ed93fcd795ed14ee475c9ba7bd
Klas <31806893+klassare@users.noreply.github.com>
2018-07-10 21:23:16 +0200

    add delegator name-pipe (#2)

---
+++ src/app/pipes/delegator-name.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DelegatorNamePipe } from './delegator-name.pipe';
+
+describe('DelegatorNamePipe', () => {
+  it('create an instance', () => {
+    const pipe = new DelegatorNamePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/pipes/delegator-name.pipe.ts
@@ -0,0 +1,31 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'delegatorName'
+})
+export class DelegatorNamePipe implements PipeTransform {
+  map: Map<string, string> = new Map([
+    ['tz1TDSmoZXwVevLTEvKCTHWpomG76oC9S2fJ', 'Tezos.Community'],
+    ['tz1WCd2jm4uSt4vntk4vSuUWoZQGhLcDuR9q', 'Happy Tezos'],
+    ['tz1XQ7SRj4QQWjaeebNd8dFwuTrCot3GGDRF', 'Tz Baker'],
+    ['tz1YKh8T79LAtWxX29N5VedCSmaZGw9LNVxQ', 'Tezos Brazil'],
+    ['tz3bEQoFCZEEfZMskefZ8q8e4eiHH1pssRax', 'Ceibo XTZ'],
+    ['tz1hThMBD8jQjFt78heuCnKxJnJtQo9Ao25X', 'Tezos Chef'],
+    ['tz1L3vFD8mFzBaS8yLHFsd7qDJY1t276Dh8i', 'Zednode'],
+    ['tz1Tnjaxk6tbAeC2TmMApPh8UsrEVQvhHvx5', 'My Crypto Delegate'],
+    ['tz1LesY3S4wfe15SNm1W3qJmQzWxLqVjTruH', 'Xtez.io'],
+    ['tz1L5GqtsKbasq9yD4hvtGC7VprPXDPmeb9V', 'Tezos Bakes']
+  ]);
+  transform(pkh: string): any {
+    console.log('Get delegator name');
+    if (!pkh) {
+      return '';
+    }
+    const name = this.map.get(pkh);
+    if (name) {
+      return name;
+    } else {
+      return '';
+    }
+  }
+}
