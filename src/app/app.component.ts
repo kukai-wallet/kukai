64ef879314f827048ca45c0f5a93b25a0c947486
DM <cryptofanonawindyday@gmail.com>
2018-08-02 14:20:16 +0100

    test code transferred to homePage and Language button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {TranslateService} from '@ngx-translate/core';  // Init the TranslateService
 
 import { WalletService } from './services/wallet.service';
 import { CoordinatorService } from './services/coordinator.service';
@@ -11,22 +10,10 @@ import { CoordinatorService } from './services/coordinator.service';
 })
 export class AppComponent implements OnInit {
 
-    param = {value: 'world'};  // Test translation
-
     constructor(
-        public translate: TranslateService,
         private walletService: WalletService,
         private coordinatorService: CoordinatorService
-    ) {
-        translate.addLangs(['en', 'fr']);
-
-        // this language will be used as a fallback when a translation isn't found in the current language
-        translate.setDefaultLang('en');
-
-        // the lang to use, if the lang isn't available, it will use the current loader to get them
-        const browserLang = translate.getBrowserLang();
-        translate.use(browserLang.match(/en|fr/) ? browserLang : 'en');
-    }
+    ) { }
 
     ngOnInit() {
             this.walletService.loadStoredWallet();
--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { TranslateService } from '@ngx-translate/core';  // Init the TranslateService
+
 import { WalletService } from '../../services/wallet.service';
 import { CoordinatorService } from '../../services/coordinator.service';
 
@@ -16,13 +18,51 @@ export class HomePageComponent implements OnInit {
 
     CONSTANTS = new Constants();
 
+    param = {value: 'world'};  // Test translation
+
     constructor(
+        public translate: TranslateService,
         private router: Router,
         public walletService: WalletService,
         private coordinatorService: CoordinatorService
-    ) { }
+    ) {
+        translate.addLangs(['en', 'fr']);
+
+        // this language will be used as a fallback when a translation isn't found in the current language
+        translate.setDefaultLang('en');
+
+        // the lang to use, if the lang isn't available, it will use the current loader to get them
+        const browserLang = translate.getBrowserLang();
+        console.log('browserLang ', browserLang);
+        translate.use(browserLang.match(/en|fr/) ? browserLang : 'en');
+    }
 
   ngOnInit() {
+  }
+
+  testChange(lang: string) {
+    console.log('lang in testChange() ', lang);
+  }
+
+  returnLanguage(lang: string) {
+
+    // this.translate.use(lang);
+    // console.log('lang ', lang);
+
+    const map: Map<string, string> = new Map([
+        ['en', 'English'],
+        ['cn', '中国'],
+        ['es', 'Español'],
+        ['fr', 'Français'],
+        ['ru', 'Pусский'],
+        ['jp', '日本の'],
+        ['kr', '한국어'],
+        ['swe', 'Svenska']
+    ]);
+
+    const language = map.get(lang);
+
+    return language;
   }
 
   logout() {
