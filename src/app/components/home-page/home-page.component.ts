191b75fae6533f641cf74714df01b999eb413ae0
DM <cryptofanonawindyday@gmail.com>
2018-09-06 14:49:49 +0100

    minor change

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -26,7 +26,7 @@ export class HomePageComponent implements OnInit {
         public walletService: WalletService,
         private coordinatorService: CoordinatorService
     ) {
-        translate.addLangs(['en', 'fr', 'ru', 'jp', 'por', 'kor']);
+        translate.addLangs(['en', 'fr', 'ru', 'jp', 'kor', 'por']);
 
         // this language will be used as a fallback when a translation isn't found in the current language
         translate.setDefaultLang('en');
@@ -34,7 +34,7 @@ export class HomePageComponent implements OnInit {
         // the lang to use, if the lang isn't available, it will use the current loader to get them
         const browserLang = translate.getBrowserLang();
         console.log('browserLang ', browserLang);
-        translate.use(browserLang.match(/en|fr|ru|jp|por|kor/) ? browserLang : 'en');
+        translate.use(browserLang.match(/en|fr|ru|jp|kor|por/) ? browserLang : 'en');
     }
 
   ngOnInit() {
