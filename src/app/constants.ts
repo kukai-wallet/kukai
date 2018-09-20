03d937300d156e32b059262c91df2ac5948dc8cd
Klassare <klassare@gmail.com>
2018-09-20 14:02:59 +0200

    add alphanet

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -6,8 +6,7 @@ interface Net {
     CHAIN_ID: string;
   }
 export class Constants {
-    // Select Betanet or Zeronet
-    // readonly NET: Net = this.betanet();
+    // Select Zeronet, Alphanet or Mainnet
     readonly NET: Net = this.zeronet();
 
     zeronet(): Net {
@@ -20,14 +19,24 @@ export class Constants {
         };
         return ZERONET;
     }
-    betanet(): Net {
-        const BETANET: Net = {
-            NAME:  'Betanet',
-            API_URL:  'https://api.tzscan.io/',
+    alphanet(): Net {
+        const ALPHANET: Net = {
+            NAME:  'Alphanet',
+            API_URL:  'https://alphanet-api.tzscan.io/',
+            NODE_URL: 'https://alphanet-node.tzscan.io/',
+            BLOCK_EXPLORER_URL: 'https://alphanet.tzscan.io/',
+            CHAIN_ID: 'PsYLVpVvgbLhAhoqAkMFUo6gudkJ9weNXhUYCiLDzcUpFpkk8Wt'
+        };
+        return ALPHANET;
+    }
+    mainnet(): Net {
+        const MAINNET: Net = {
+            NAME:  'Mainnet',
+            API_URL:  'https://api6.tzscan.io/',
             NODE_URL: 'https://rpc.tezrpc.me/',
             BLOCK_EXPLORER_URL: 'https://tzscan.io/',
             CHAIN_ID: 'PsYLVpVvgbLhAhoqAkMFUo6gudkJ9weNXhUYCiLDzcUpFpkk8Wt'
         };
-        return BETANET;
+        return MAINNET;
     }
 }
