1a4906fd360780d5ddd4ed9813b415a855f0fb03
Klassare <klassare@gmail.com>
2019-01-20 17:25:01 +0100

    set alphanet

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -7,9 +7,9 @@ interface Net {
   }
 export class Constants {
     // Select Zeronet, Alphanet or Mainnet
-    // readonly NET: Net = this.alphanet();
+    readonly NET: Net = this.alphanet();
     // readonly NET: Net = this.zeronet();
-    readonly NET: Net = this.mainnet();
+    // readonly NET: Net = this.mainnet();
 
     zeronet(): Net {
         const ZERONET: Net = {
