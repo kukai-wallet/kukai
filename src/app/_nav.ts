f624ae051f00206ba75e0d4aa3f52d44dda9094c
Corey Soreff <csoreff@gmail.com>
2020-04-20 20:48:36 +0900

    UI Overhaul Part 1

---
+++ src/app/_nav.ts
@@ -0,0 +1,55 @@
+import { INavData } from '@coreui/angular';
+
+export const sidebarNavItems: INavData[] = [
+  {
+    name: 'Dashboard',
+    url: '/overview',
+    icon: 'icon-speedometer'
+  },
+  {
+    name: 'Account',
+    url: '/account',
+    icon: 'icon-calculator'
+  },
+  {
+    name: 'Bakery',
+    url: '/bakery',
+    icon: 'icon-pie-chart'
+  },
+  {
+    title: true,
+    name: 'Options'
+  },
+  {
+    name: 'Offline Signing',
+    url: '/offline-signing',
+    icon: 'icon-note',
+  },
+  {
+    name: 'Backup',
+    url: '/backup',
+    icon: 'icon-paper-clip',
+  },
+  {
+    name: 'Github',
+    url: 'https://github.com/kukai-wallet/kukai',
+    icon: 'icon-social-github',
+    class: 'mt-auto',
+    variant: 'dark',
+    attributes: { target: '_blank', rel: 'noopener' }
+  },
+  {
+    name: 'Chat',
+    url: 'https://riot.im/app/#/room/#kukai:matrix.org',
+    icon: 'icon-bubbles',
+    variant: 'success',
+    attributes: { target: '_blank', rel: 'noopener' }
+  },
+  {
+    name: 'Twitter',
+    url: 'https://twitter.com/KukaiWallet/',
+    icon: 'icon-social-twitter',
+    variant: 'primary',
+    attributes: { target: '_blank', rel: 'noopener' }
+  }
+];
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,24 +1,73 @@
+/**
+ * This file includes polyfills needed by Angular and is loaded before the app.
+ * You can add your own extra polyfills to this file.
+ *
+ * This file is divided into 2 sections:
+ *   1. Browser polyfills. These are applied before loading ZoneJS and are sorted by browsers.
+ *   2. Application imports. Files imported after ZoneJS that should be loaded before your main
+ *      file.
+ *
+ * The current setup is for so-called "evergreen" browsers; the last versions of browsers that
+ * automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera),
+ * Edge >= 13 on the desktop, and iOS 10 and Chrome on mobile.
+ *
+ * Learn more in https://angular.io/docs/ts/latest/guide/browser-support.html
+ */
+
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+// for IE10
+import 'mutationobserver-shim';
 
 /**
  * Required to support Web Animations `@angular/platform-browser/animations`.
  * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
  **/
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
+/**
+ * By default, zone.js will patch all possible macroTask and DomEvents
+ * user can disable parts of macroTask/DomEvents patch by setting following flags
+ */
 
+ (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
+ (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
+ (window as any).__zone_symbol__BLACK_LISTED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
+
+ /*
+ * in IE/Edge developer tools, the addEventListener will also be wrapped by zone.js
+ * with the following flag, it will bypass `zone.js` patch for IE/Edge
+ */
+(window as any).__Zone_enable_cross_context_check = true;
 
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-(window as any).global = window;
-global.Buffer = global.Buffer || require('buffer').Buffer;
-(window as any).process = {
-    env: { DEBUG: undefined },
-    version: '1'
-  };
+
 
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+/***************************************************************************************************
+ * IE10 issues
+ */
+
+/**
+ * IE10 - some CSS3 properties and HTML5 elements are not fully supported or require prefixed properties for full functionality
+ * to let Autoprefixer use old flexbox prefixes - add a browserslist property to the package.json file:
+ * "browserslist": [
+ * "last 2 versions",
+ * "not ie < 10",
+ * ]
+ */
+
+/**
+ * IE10 - TypeError: Object doesn't support property or method 'getRootNode'
+ */
+// import 'get-root-node-polyfill/implement'; // Run `npm i get-root-node-polyfill`.
+
