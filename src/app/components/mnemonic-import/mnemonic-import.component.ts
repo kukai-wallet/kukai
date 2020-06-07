2d84bd2c48ce9bd908e5c9d3d006b7795ef0fdb1
Klassare <klassare@gmail.com>
2020-06-07 19:05:34 +0200

    add remaining import views

--- src/app/components/mnemonic-import/mnemonic-import.component.ts
+++ src/app/components/mnemonic-import/mnemonic-import.component.ts
@@ -1,21 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Constants } from '../../constants';
-import { TranslateService } from '@ngx-translate/core';  // Multiple instances created ?
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Constants } from "../../constants";
+import { TranslateService } from "@ngx-translate/core"; // Multiple instances created ?
 
-import { ImportService } from '../../services/import/import.service';
-import { MessageService } from '../../services/message/message.service';
-import { OperationService } from '../../services/operation/operation.service';
-import { WalletService } from '../../services/wallet/wallet.service';
-import { CoordinatorService } from '../../services/coordinator/coordinator.service';
-import { ExportService } from '../../services/export/export.service';
-import { InputValidationService } from '../../services/input-validation/input-validation.service';
-import { utils, hd } from '@tezos-core-tools/crypto-utils';
+import { ImportService } from "../../services/import/import.service";
+import { MessageService } from "../../services/message/message.service";
+import { WalletService } from "../../services/wallet/wallet.service";
+import { ExportService } from "../../services/export/export.service";
+import { InputValidationService } from "../../services/input-validation/input-validation.service";
+import { utils, hd } from "@tezos-core-tools/crypto-utils";
 
 @Component({
-  selector: 'app-mnemonic-import-wallet',
-  templateUrl: './mnemonic-import.component.html',
-  styleUrls: ['./mnemonic-import.component.scss']
+  selector: "app-mnemonic-import-wallet",
+  templateUrl: "./mnemonic-import.component.html",
+  styleUrls: ["./mnemonic-import.component.scss"],
 })
 export class MnemonicImportComponent implements OnInit {
   CONSTANTS = new Constants();
@@ -25,15 +23,17 @@ export class MnemonicImportComponent implements OnInit {
   password: string;
   passphrase: string;
   pkh: string;
+  importOption = 0;
   activePanel = 0;
-  tge = false;
-  hdImport = true;
+  hdImport = false;
   wallet: any;
+  walletJson: string;
+  pwd = '';
   pwd1: string;
   pwd2: string;
-  pwdStrength = '';
+  pwdStrength = "";
   Downloaded = false;
-
+  fileName = '';
   showWrongFileUploadMsg: false;
 
   constructor(
@@ -41,58 +41,63 @@ export class MnemonicImportComponent implements OnInit {
     private importService: ImportService,
     private router: Router,
     private messageService: MessageService,
-    private operationService: OperationService,
     private walletService: WalletService,
-    private coordinatorService: CoordinatorService,
     private exportService: ExportService,
     private inputValidationService: InputValidationService
-  ) { }
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   retrieve() {
-    if (this.tge) {
+    if (this.importOption === 2) {
       this.passphrase = this.email + this.password;
     }
     if (!this.inputValidationService.mnemonics(this.mnemonic)) {
-      this.translate.get('MNEMONICIMPORTCOMPONENT.INVALIDMNEMONIC').subscribe(
-        (res: string) => this.messageService.addWarning(res, 10)
+      this.translate
+        .get("MNEMONICIMPORTCOMPONENT.INVALIDMNEMONIC")
+        .subscribe((res: string) => this.messageService.addWarning(res, 10));
+    } else if (
+      this.importOption === 2 &&
+      !this.inputValidationService.email(this.email)
+    ) {
+      this.translate.get("MNEMONICIMPORTCOMPONENT.INVALIDEMAIL").subscribe(
+        (res: string) => this.messageService.addWarning(res, 10) // 'Invalid email!'
       );
-    } else if (this.tge && !this.inputValidationService.email(this.email)) {
-      this.translate.get('MNEMONICIMPORTCOMPONENT.INVALIDEMAIL').subscribe(
-        (res: string) => this.messageService.addWarning(res, 10)  // 'Invalid email!'
-      );
-    } else if (this.tge && !this.password) {
-      this.translate.get('MNEMONICIMPORTCOMPONENT.INVALIDPASSWORD').subscribe(
-        (res: string) => this.messageService.addWarning(res, 10)  // 'Invalid password!'
+    } else if (this.importOption === 2 && !this.password) {
+      this.translate.get("MNEMONICIMPORTCOMPONENT.INVALIDPASSWORD").subscribe(
+        (res: string) => this.messageService.addWarning(res, 10) // 'Invalid password!'
       );
     } else if (!this.inputValidationService.passphrase(this.passphrase)) {
-      this.translate.get('MNEMONICIMPORTCOMPONENT.INVALIDPASSPHRASE').subscribe(
-        (res: string) => this.messageService.addWarning(res, 10)  // 'Invalid passphrase!'
+      this.translate.get("MNEMONICIMPORTCOMPONENT.INVALIDPASSPHRASE").subscribe(
+        (res: string) => this.messageService.addWarning(res, 10) // 'Invalid passphrase!'
       );
     } else if (this.pkh && !this.inputValidationService.address(this.pkh)) {
-      this.translate.get('MNEMONICIMPORTCOMPONENT.INVALIDPKH').subscribe(
-        (res: string) => this.messageService.addWarning(res, 10)  // 'Invalid public key hash!'
+      this.translate.get("MNEMONICIMPORTCOMPONENT.INVALIDPKH").subscribe(
+        (res: string) => this.messageService.addWarning(res, 10) // 'Invalid public key hash!'
       );
     } else {
-      let pkh = '';
+      let pkh = "";
       if (this.pkh) {
-        if (!this.tge && this.hdImport) {
-          pkh = hd.keyPairFromAccountIndex(utils.mnemonicToSeed(this.mnemonic, this.passphrase, true), 0).pkh;
+        if (this.importOption === 1 && this.hdImport) {
+          pkh = hd.keyPairFromAccountIndex(
+            utils.mnemonicToSeed(this.mnemonic, this.passphrase, true),
+            0
+          ).pkh;
         } else {
-          pkh = utils.seedToKeyPair(utils.mnemonicToSeed(this.mnemonic, this.passphrase, false)).pkh;
+          pkh = utils.seedToKeyPair(
+            utils.mnemonicToSeed(this.mnemonic, this.passphrase, false)
+          ).pkh;
         }
       }
       if (this.pkh && pkh !== this.pkh) {
-        if (this.tge) {
-          this.translate.get('MNEMONICIMPORTCOMPONENT.INVALIDEMAILPASSWORD').subscribe(
-            (res: string) => this.messageService.addWarning(res, 5)
-          );
+        if (this.importOption === 2) {
+          this.translate
+            .get("MNEMONICIMPORTCOMPONENT.INVALIDEMAILPASSWORD")
+            .subscribe((res: string) => this.messageService.addWarning(res, 5));
         } else {
-          this.translate.get('MNEMONICIMPORTCOMPONENT.INVALIDPASSPHRASE').subscribe(
-            (res: string) => this.messageService.addWarning(res, 5)
-          );
+          this.translate
+            .get("MNEMONICIMPORTCOMPONENT.INVALIDPASSPHRASE")
+            .subscribe((res: string) => this.messageService.addWarning(res, 5));
         }
       } else {
         this.activePanel++;
@@ -102,34 +107,31 @@ export class MnemonicImportComponent implements OnInit {
   setPwd() {
     if (this.validPwd()) {
       const password = this.pwd1;
-      this.pwd1 = '';
-      this.pwd2 = '';
-      this.wallet = this.walletService.createEncryptedWallet(this.mnemonic, password, this.passphrase, (!this.tge && this.hdImport));
-      this.mnemonic = '';
-      this.passphrase = '';
-      this.email = '';
-      this.password = '';
+      this.pwd1 = "";
+      this.pwd2 = "";
+      this.wallet = this.walletService.createEncryptedWallet(
+        this.mnemonic,
+        password,
+        this.passphrase,
+        this.importOption === 1 && this.hdImport
+      );
+      this.mnemonic = "";
+      this.passphrase = "";
+      this.email = "";
+      this.password = "";
       this.activePanel++;
     }
   }
 
-  validateFile(name: String) {
-    const ext = name.substring(name.lastIndexOf('.') + 1);
-    if (ext.toLowerCase() === 'json') {
-      return true;
-    } else {
-      return false;
-    }
-  }
   validPwd(): boolean {
     if (!this.inputValidationService.password(this.pwd1)) {
-      this.translate.get('MNEMONICIMPORTCOMPONENT.PASSWORDWEAK').subscribe(
-        (res: string) => this.messageService.addWarning(res, 10)  // 'Password is too weak!'
+      this.translate.get("MNEMONICIMPORTCOMPONENT.PASSWORDWEAK").subscribe(
+        (res: string) => this.messageService.addWarning(res, 10) // 'Password is too weak!'
       );
       return false;
     } else if (this.pwd1 !== this.pwd2) {
-      this.translate.get('MNEMONICIMPORTCOMPONENT.NOMATCHPASSWORDS').subscribe(
-        (res: string) => this.messageService.addWarning(res, 10)  // Passwords don't match!
+      this.translate.get("MNEMONICIMPORTCOMPONENT.NOMATCHPASSWORDS").subscribe(
+        (res: string) => this.messageService.addWarning(res, 10) // Passwords don't match!
       );
       return false;
     } else {
@@ -137,7 +139,9 @@ export class MnemonicImportComponent implements OnInit {
     }
   }
   calcStrength() {
-    this.pwdStrength = this.inputValidationService.passwordStrengthDisplay(this.pwd1);
+    this.pwdStrength = this.inputValidationService.passwordStrengthDisplay(
+      this.pwd1
+    );
   }
   export(): string {
     return JSON.stringify(this.wallet.data);
@@ -150,14 +154,123 @@ export class MnemonicImportComponent implements OnInit {
     this.Downloaded = true;
   }
   async done() {
-    await this.importService.importWalletFromObject(this.wallet.data, this.wallet.seed);
-    this.wallet = null;
-    this.router.navigate(['/accounts']);
-    this.translate.get('MNEMONICIMPORTCOMPONENT.WALLETREADY').subscribe(
-      (res: string) => this.messageService.addSuccess(res)
+    await this.importService.importWalletFromObject(
+      this.wallet.data,
+      this.wallet.seed
     );
+    this.wallet = null;
+    this.router.navigate(["/accounts"]);
+    this.translate
+      .get("MNEMONICIMPORTCOMPONENT.WALLETREADY")
+      .subscribe((res: string) => this.messageService.addSuccess(res));
   }
-  tabChanged() {
-      this.tge = !this.tge;
+  /* Keystore handling */
+  importPreCheck(keyFile: string) {
+    this.typeCheckFile(keyFile);
+    if (this.importService.pwdRequired(keyFile)) {
+      this.walletJson = keyFile;
+    } else {
+      throw new Error("Unsupported wallet file");
+    }
+  }
+  typeCheckFile(keyFile: string) {
+    const obj = JSON.parse(keyFile);
+    // Required
+    try {
+      if (typeof obj.provider !== "string") {
+        throw new Error("provider not a string!");
+      }
+      if (typeof obj.version !== "number") {
+        throw new Error("version not a number!");
+      }
+      if (typeof obj.walletType !== "number") {
+        throw new Error("walletType not a number!");
+      }
+      // Optional
+      if (obj.encryptedSeed && typeof obj.encryptedSeed !== "string") {
+        throw new Error("encryptedSeed not a string!");
+      }
+      if (obj.pkh && typeof obj.pkh !== "string") {
+        throw new Error("pkh not a string!");
+      }
+      if (obj.iv && typeof obj.iv !== "string") {
+        throw new Error("iv not a string!");
+      }
+      if (obj.pk && typeof obj.pk !== "string") {
+        throw new Error("pk not a string!");
+      }
+      if (obj.encryptedEntropy && typeof obj.encryptedEntropy !== "string") {
+        throw new Error("encryptedEntropy not a string!");
+      }
+    } catch (e) {
+      this.messageService.addError(e);
+      throw e;
+    }
+  }
+  checkImportPwd() {
+    if (this.pwd) {
+      this.import(this.walletJson, this.pwd);
+      this.pwd = "";
+    }
+  }
+  import(keyFile: string, pwd: string) {
+    this.typeCheckFile(keyFile);
+    this.importService
+      .importWalletFromJson(keyFile, pwd)
+      .then((success: boolean) => {
+        if (success) {
+          this.router.navigate(["/accounts"]);
+        } else {
+          let importFailed = "";
+          this.translate
+            .get("IMPORTCOMPONENT.IMPORTFAILED")
+            .subscribe((res: string) => (importFailed = res));
+          this.messageService.add(importFailed);
+        }
+      });
+  }
+  handleFileInput(files: FileList) {
+    let fileToUpload = files.item(0);
+    if (!this.validateFile(fileToUpload.name)) {
+      let fileNotSupported = "";
+      this.translate
+        .get("IMPORTCOMPONENT.FILENOTSUPPORTED")
+        .subscribe((res: string) => (fileNotSupported = res));
+      this.messageService.add(fileNotSupported);
+
+      console.log("Selected file format is not supported");
+      fileToUpload = null;
+      this.walletJson = null;
+      return false;
+    } else {
+
+      const reader = new FileReader();
+      reader.readAsText(fileToUpload);
+      reader.onload = () => {
+        if (typeof reader.result === "string") {
+          console.log(reader.result);
+          try {
+            this.importPreCheck(reader.result);
+          } catch (e) {
+            this.messageService.addError(e, 5);
+            this.walletJson = null;
+          }
+          if (this.walletJson) {
+            this.fileName = fileToUpload.name;
+          }
+        } else {
+          this.walletJson = null;
+          throw new Error("Not a string import");
+        }
+      };
+    }
+  }
+  validateFile(name: String) {
+    const ext = name.substring(name.lastIndexOf(".") + 1);
+    if (ext.toLowerCase() === "tez") {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
