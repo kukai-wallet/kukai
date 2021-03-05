a749116bdb00bc3c9e39846addfcf2f4d6d0aa00
Charles Cunningham <c.a.cunningham6@gmail.com>
2021-03-05 16:24:47 +0100

    get right icon for verifier

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { WalletService } from '../../services/wallet/wallet.service';
-import { TorusWallet } from '../../services/wallet/wallet';
+import {
+  TorusWallet,
+  ImplicitAccount
+} from '../../services/wallet/wallet';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -21,18 +24,25 @@ export class CardComponent implements OnInit {
       .filter(params => params.instanceId)
       .subscribe(params => {
         this.walletService.loadStoredWallet(params.instanceId);
-        if (this.walletService.wallet instanceof EmbeddedTorusWallet) {
+        if (this.walletService.wallet instanceof TorusWallet) {
           this.activeAccount = this.walletService.wallet.implicitAccounts[0];
         }
-      }
-      );
+      });
   }
 
   displayName(): string {
     if (this.walletService.wallet instanceof TorusWallet) {
       return this.walletService.wallet.displayName();
     } else {
-      return 'fake_name';
+      return '';
+    }
+  }
+
+  verifier(): string {
+    if (this.walletService.wallet instanceof TorusWallet) {
+      return this.walletService.wallet.verifier;
+    } else {
+      return '';
     }
   }
 }
