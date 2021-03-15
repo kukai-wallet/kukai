1bb4c56d66d967776c933a94c88d6aded36455c6
Klassare <klassare@gmail.com>
2021-03-15 08:34:51 +0100

    fix storeKey

--- src/app/services/wallet/wallet.service.ts
+++ src/app/services/wallet/wallet.service.ts
@@ -20,7 +20,7 @@ import { utils, hd } from '@tezos-core-tools/crypto-utils';
 
 @Injectable()
 export class WalletService {
-  storeKey: 'kukai-wallet';
+  storeKey: string = 'kukai-wallet';
   storageId = 0;
   wallet: WalletObject;
 
