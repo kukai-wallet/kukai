aaff4d7709eac3eea637d0d5ea0fa79d4297ba79
Klassare <klassare@gmail.com>
2021-07-14 22:27:33 +0200

    custom expr width

--- src/app/components/embedded/confirm-send/confirm-send.component.ts
+++ src/app/components/embedded/confirm-send/confirm-send.component.ts
@@ -22,8 +22,7 @@ export class ConfirmSendEmbedComponent implements OnInit, OnChanges {
 
   ngOnInit(): void {
     this.messageService.origin.pipe(take(1)).subscribe((origin) => {
-      console.log(origin);
-      if (origin.indexOf('minterpop') !== -1) {
+      if (origin?.indexOf('minterpop') !== -1) {
         this.template = 'minterpop';
       } else {
         this.template = 'default';
