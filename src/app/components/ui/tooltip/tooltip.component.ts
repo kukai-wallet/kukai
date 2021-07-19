6acb7ef6dfe210a7ebe778d0b84558e5d3f7b554
maelswarm <roecrew@gmail.com>
2021-07-19 17:07:50 -0500

    input[type=radio] -- safari test-0

---
+++ src/app/components/ui/tooltip/tooltip.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-tooltip',
+  templateUrl: './tooltip.component.html',
+  styleUrls: ['../../../../../scss/components/ui/tooltip.component.scss']
+})
+
+export class TooltipComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
