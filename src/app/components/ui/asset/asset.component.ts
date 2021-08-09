717b288441215ced07951c7a8097c9381f57927b
Brandon Barber <roecrew@gmail.com>
2021-08-09 06:49:44 -0400

    send-confirm: img correction when sending tez

--- src/app/components/ui/asset/asset.component.ts
+++ src/app/components/ui/asset/asset.component.ts
@@ -11,9 +11,9 @@ export class AssetComponent implements OnInit, OnChanges {
 
   @Input() meta;
   @Input() size = '150x150';
+  @Input() src = undefined;
   baseUrl = 'https://backend.kukai.network/file';
   mimeType = 'image/*';
-  src = '../../../../assets/img/question-mark.svg';
 
   constructor() { }
 
@@ -30,7 +30,7 @@ export class AssetComponent implements OnInit, OnChanges {
     if (this.meta?.Status === 'ok') {
       this.mimeType = Object.keys(mimes).filter(key => !!mimes[key]?.extensions?.length).find((key) => mimes[key].extensions.includes(this.meta?.Extension));
       this.src = `${this.baseUrl}/${this.meta.Filename}_${this.size}.${this.meta.Extension}`;
-    } else {
+    } else if(!this.src) {
       this.mimeType = 'image/*';
       this.src = '../../../../assets/img/question-mark.svg';
     }
