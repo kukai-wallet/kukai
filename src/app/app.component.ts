ffbfcc5128a18f005d6cfcd61e453a6b5eeffbc7
Brandon Barber <roecrew@gmail.com>
2021-08-18 17:35:17 -0500

    overscroll test-4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router, Event, NavigationEnd } from '@angular/router';
 import { WalletService } from './services/wallet/wallet.service';
 import { TranslateService } from '@ngx-translate/core';
@@ -13,6 +13,11 @@ import { CONSTANTS as _CONSTANTS } from '../environments/environment';
 export class AppComponent implements OnInit {
   readonly CONSTANTS = _CONSTANTS;
   embedded = false;
+  previous = 0;
+  current = 0;
+  diff = 0;
+  container = null;
+  post = false;
   constructor(
     private walletService: WalletService,
     public router: Router,
@@ -85,5 +90,44 @@ export class AppComponent implements OnInit {
   setLanguage(lang) {
     window.localStorage.setItem('languagePreference', lang);
     this.translate.use(lang);
+  }
+
+  @HostListener('touchstart', ['$event'])
+  touchstart(e) {
+    this.container = e.target.closest('.nfts .scroll .body, .scroll-wrapper .balances');
+    if(!!this.container) {
+      this.container.style.overflowY = '';
+    }
+    this.post = false;
+    this.previous = 0;
+    this.current = e.touches[0].pageY;
+  }
+
+  @HostListener('touchmove', ['$event'])
+  touchmove(e) {
+    this.previous = this.current;
+    this.current = e.changedTouches[0].pageY;
+      if (this.previous > this.current && !!this.container && this.container?.scrollTop >= (this.container.scrollHeight - this.container.clientHeight)) {
+        this.container.style.overflowY = 'hidden';
+        this.container.scrollTop = this.container.scrollHeight - this.container.clientHeight;
+        document.body.scrollTop += (this.previous - this.current);
+        this.post = true;
+      } else if (this.previous < this.current && !!this.container && this.container?.scrollTop <= 0) {
+        this.container.style.overflowY = 'hidden';
+        this.container.scrollTop = -1;
+        document.body.scrollTop += (this.previous - this.current);
+        this.post = true;
+      } else {
+        if(!!this.container && this.post) {
+          document.body.scrollTop += (this.previous - this.current);
+        }
+    }
+  }
+
+  @HostListener('touchend', ['$event'])
+  touchend(e) {
+    if(!!this.container) {
+      this.container.style.overflowY = '';
+    }
   }
 }
