4ade2e1d6532b05e72812e3279ae33af9c2f811e
Klassare <klassare@gmail.com>
2021-09-07 16:46:13 +0200

    fix patch replace

--- patch.js
+++ patch.js
@@ -7,8 +7,8 @@ fs.readFile(f, 'utf8', function (err, data) {
   }
   if (data.indexOf('new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)') === -1) {
     var result = data.replace(/node: false/g, 'node: {crypto: true, stream: true, fs: \'empty\'}');
-    result = result.replace('...extraPlugins', 'new webpack.IgnorePlugin(/^\\.\\/locale$/, /moment$/), ...extraPlugins');
-    result = result.replace('const plugins_1 = require("../plugins");', 'const webpack = require("webpack"); const plugins_1 = require("../plugins");');
+    result = result.replace('  ...extraPlugins', '  new webpack.IgnorePlugin(/^\\.\\/locale$/, /moment$/), ...extraPlugins');
+    result = result.replace('\nconst plugins_1 = require("../plugins");', '\nconst webpack = require("webpack"); const plugins_1 = require("../plugins");');
 
     fs.writeFile(f, result, 'utf8', function (err) {
       if (err) return console.log(err);
