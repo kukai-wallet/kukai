b6acc2071de8e9d31314477477d164e4642f8bb7
AlcisBragi <93207547+AlcisBragi@users.noreply.github.com>
2021-10-26 21:44:02 -0500

    Adding Hash Three Points & GOGOs collection (#186)
    * Adding Project Gogo so it displays nicely
    
    * Adding GOGOs to trusted tokens
    
    * Adding H3P collection info to display
    
    Co-authored-by: Klas Harrysson <31806893+klassare@users.noreply.github.com>
    Co-authored-by: Gabriel Guzman <gguzman@insum.ca>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -173,6 +173,18 @@ export const CONSTANTS: Constants = {
         address: ["KT1MsdyBSAMQwzvDH4jt2mxUKJvBSWZuPoRJ"],
         thumbnailUrl: "../../../assets/img/alias/neonz.jpg",
         link: "https://neonz.xyz", shouldDisplayLink: DisplayLinkOption.None
+    },
+    'GOGOs': {
+        name: "GOGOs",
+        address: ["KT1SyPgtiXTaEfBuMZKviWGNHqVrBBEjvtfQ"],
+        thumbnailUrl: "../../../assets/img/alias/gogo.jpg",
+        link: "https://gogos.tez.page/", shouldDisplayLink: DisplayLinkOption.None
+    },
+    'HashThreePoints': {
+        name: "Hash Three Points",
+        address: ["KT1Fxz4V3LaUcVFpvF8pAAx8G3Z4H7p7hhDg"],
+        thumbnailUrl: "../../../assets/img/alias/hashthreepoints.png",
+        link: "https://h3p.deconcept.com/", shouldDisplayLink: DisplayLinkOption.None
     }
   },
   ASSETS: {
@@ -904,6 +916,8 @@ export const TRUSTED_TOKEN_CONTRACTS = [
   'KT1LbLNTTPoLgpumACCBFJzBEHDiEUqNxz5C',//Art Cardz
   'KT1VbHpQmtkA3D4uEbbju26zS8C42M5AGNjZ',//PRJKTNEON
   'KT1H8sxNSgnkCeZsij4z76pkXu8BCZNvPZEx',//PRJKTNEON FILES
-  'KT1MsdyBSAMQwzvDH4jt2mxUKJvBSWZuPoRJ'//NEONZ
+  'KT1MsdyBSAMQwzvDH4jt2mxUKJvBSWZuPoRJ',//NEONZ
+  'KT1SyPgtiXTaEfBuMZKviWGNHqVrBBEjvtfQ',//GOGOs
+  'KT1Fxz4V3LaUcVFpvF8pAAx8G3Z4H7p7hhDg' //Hash Three Points
 ];
 export const BLACKLISTED_TOKEN_CONTRACTS = []
