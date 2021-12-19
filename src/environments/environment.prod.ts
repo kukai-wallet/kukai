f534eb6032ae2a1d96ef18061a9fa878ee26bdc5
Alexis Salnikoff <salnikoff.a@gmail.com>
2021-12-19 22:01:14 +0100

    add skratz basq (#236)
    * add skratz basq
    
    * update asset

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -327,6 +327,14 @@ export const CONSTANTS: Constants = {
       category: ["collectibles"],
       description: "In 2420, only 6666 kids survived the destruction of the global ecosystem caused by the excessive minting of dirty NFTs on proof of work blockchains. CyberKidz Club.",
     },
+    'SkratzBasqTribute': {
+      name: "Skratz Basq",
+      address: ["KT1LaGxGqGCE7wRRP6NpZ9CSwq78y5PS6udh"],
+      thumbnailUrl: "../../../assets/img/alias/skratz.jpeg",
+      link: "https://houseofskratz.com", shouldDisplayLink: DisplayLinkOption.None,
+      category: ["collectibles"],
+      description: "Skratz Basq Tribute from https://houseofskratz.com.",
+    },
     'TezApeGang': {
       name: "TezApeGang",
       address: ["KT1FReMp4U1KipyH53xXUnnjtdRQZaLnQpUj"],
@@ -1090,6 +1098,7 @@ export const TRUSTED_TOKEN_CONTRACTS = [
   'KT1TnVQhjxeNvLutGvzwZvYtC7vKRpwPWhc6',// Ubisoft Quartz
   'KT1AFxcFeTyrgmULfBwvo9oCSkTwi5TZb2hm',//Pixel Panda
   'KT1DuZFNv6JwwA7kS8RXvoQiANm3L4dfn6qP',//CyberKidz Club
+  'KT1LaGxGqGCE7wRRP6NpZ9CSwq78y5PS6udh',//Skratz Basq
   'KT1FReMp4U1KipyH53xXUnnjtdRQZaLnQpUj'//TezApeGang
 ];
 export const BLACKLISTED_TOKEN_CONTRACTS = []
