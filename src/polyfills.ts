ca743f6c14ff9f22147e3c2bc4f1a294f36ddd0a
Brandon Barber <6314185+maelswarm@users.noreply.github.com>
2022-01-26 21:00:06 -0600

    feat: ng v13.1.3

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -51,6 +51,8 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
  * APPLICATION IMPORTS
  */
 
+(window as any).global = window;
+
 /***************************************************************************************************
  * IE10 issues
  */
