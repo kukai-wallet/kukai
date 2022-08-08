3052a682dc3054a75d5d3e7cd33775b3b1cc0b43
Klas Harrysson <31806893+klassare@users.noreply.github.com>
2022-08-08 18:09:56 +0200

    add contract address to vitality grouping

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -76,7 +76,7 @@ const _CONSTANTS: Constants = {
     },
     'vitality': {
       name: "V.Hive by Vitality",
-      address: ["KT1L6BTeGP5NcVmRjys85EaDxBymxMyx5rj8", "KT1MxZTs3QrWffkGCLrkw8hVXxBiSbqo3JZn"],
+      address: ["KT1L6BTeGP5NcVmRjys85EaDxBymxMyx5rj8", "KT1MxZTs3QrWffkGCLrkw8hVXxBiSbqo3JZn", "KT1NpS5Kfn4foSj2rKDwYraFHADq8eNuxpQS"],
       thumbnailUrl: "../../../assets/img/alias/vitality.png",
       link: "", shouldDisplayLink: DisplayLinkOption.None,
     },
