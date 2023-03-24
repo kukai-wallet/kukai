e5522b408d4cb6d307cdd444c1710687fb4c6870
Klassare <31806893+klassare@users.noreply.github.com>
2023-03-24 17:14:45 +0100

    update manu contracts

--- src/environments/environment.mainnet.ts
+++ src/environments/environment.mainnet.ts
@@ -39,7 +39,8 @@ const _CONSTANTS: Constants = {
         'KT1V7QCmuKpGsThwCNRALmsVfDAYopV98EEL',
         'KT1JUt1DNTsZC14KAxdSop34TWBZhvZ7P9a3',
         'KT1FE8NtDa7snc9nrQMx9NpfoU98uEj52tyz',
-        'KT1XNJ67F3JN2cmq6s1LmqtVg7gy9tCcN4E2'
+        'KT1XNJ67F3JN2cmq6s1LmqtVg7gy9tCcN4E2',
+        'KT1NjcoGPVVbY8QE4TTRQ4Lupu3Uu3eETZ3p'
       ],
       thumbnailUrl: 'assets/img/alias/mufc.png',
       discoverUrl: 'assets/img/alias/mufc-discover.jpg',
