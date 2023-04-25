0e80a6f828b5486df7a3cfc737c3a89f4b74d25f
mchaver <mchaver@gmail.com>
2023-04-25 15:30:29 +0200

    Feat/simplify hd and legacy wallet import
    Co-authored-by: James <james@hummingbot.io>
    Co-authored-by: Klassare <31806893+klassare@users.noreply.github.com>

--- src/app/services/utils/utils.service.ts
+++ src/app/services/utils/utils.service.ts
@@ -8,4 +8,15 @@ export class UtilsService {
   async sleep(ms: number) {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
+
+  promiseWithTimeout<T>(promise: Promise<T>, timeout: number): Promise<T> {
+    return Promise.race([
+      promise,
+      new Promise<T>((_, reject) => {
+        setTimeout(() => {
+          reject(new Error(`Timeout after ${timeout}ms`));
+        }, timeout);
+      })
+    ]);
+  }
 }
