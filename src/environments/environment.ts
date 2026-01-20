bd1d68b68a1af14a884252cf93be385bbe8df037
Klassare <31806893+klassare@users.noreply.github.com>
2026-01-20 11:37:22 +0100

    restore env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 import { Constants } from './../app/interfaces';
-import { CONSTANTS as _CONSTANTS, TRUSTED_TOKEN_CONTRACTS as _TTC } from './environment.tallinnnet';
+import { CONSTANTS as _CONSTANTS, TRUSTED_TOKEN_CONTRACTS as _TTC } from './environment.ghostnet';
 
 export const environment = {
   production: false
